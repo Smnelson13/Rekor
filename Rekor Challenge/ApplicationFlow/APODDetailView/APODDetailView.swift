276d35f8c4cfd12b6ded2451ca7be947f2fabdb4
Shane Nelson <swiftyshane@gmail.com>
2022-01-18 20:40:59 -0500

    added image scale

--- Rekor Challenge/ApplicationFlow/APODDetailView/APODDetailView.swift
+++ Rekor Challenge/ApplicationFlow/APODDetailView/APODDetailView.swift
@@ -11,7 +11,10 @@ struct APODDetailView: View {
     
     let apod: APOD
     let imageSize: CGFloat = 300
+    
     @StateObject private var store = APODDetailViewStore()
+    @State private var currentScale: CGFloat = 0
+    @State private var finalScale: CGFloat = 1
     
     var body: some View {
         ScrollView {
@@ -29,7 +32,17 @@ struct APODDetailView: View {
                     } else {
                         ProgressView().frame(width: imageSize, height: imageSize)
                     }
-                }
+                }.scaleEffect(finalScale + currentScale)
+                    .gesture(
+                        MagnificationGesture().onChanged({ scale in
+                            currentScale = scale
+                        })
+                            .onEnded({ scale in
+                                finalScale = scale
+                                currentScale = 0
+                            })
+                    )
+                
                 VStack(alignment: .center, spacing: 15) {
                     Text(apod.title).font(.headline)
                     Text(apod.explanation).font(.footnote).padding()
